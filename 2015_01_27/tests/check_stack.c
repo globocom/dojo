bd0283186cfa340dc78c9f9697f669469c9134a0
Rafael Cunha de Almeida <rafael.cunha@corp.globo.com>
2015-01-27 01:14:14 -0200

    Add testing skeleton

--- 2015_01_27/tests/check_stack.c
+++ 2015_01_27/tests/check_stack.c
@@ -1,6 +1,46 @@
+#include <stdlib.h>
+#include <check.h>
 #include "../src/stack.h"
+
+void setup(void)
+{
+}
+
+void teardown(void)
+{
+}
+
+START_TEST(test_create_stack)
+{
+}
+END_TEST
+
+Suite* stack_suite(void)
+{
+    Suite *s;
+    TCase *tc_core;
+
+    s = suite_create("Stack");
+    tc_core = tcase_create("Core Functions");
+    tcase_add_checked_fixture(tc_core, setup, teardown);
+    tcase_add_test(tc_core, test_create_stack);
+
+    suite_add_tcase(s, tc_core);
+
+    return s;
+}
 
 int main(void)
 {
-    return 0;
+    int number_failed;
+    Suite *s;
+    SRunner *sr;
+
+    s = stack_suite();
+    sr = srunner_create(s);
+
+    srunner_run_all(sr, CK_NORMAL);
+    number_failed = srunner_ntests_failed(sr);
+    srunner_free(sr);
+    return (number_failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
 }
