b7ea33dd2f6fb6f6e528d85f1941d2bc26e5ee0e
Hugo Roque <hugolnx@gmail.com>
2018-01-18 10:52:36 -0200

    Complete solution of 2018-01-17 problem

--- 2018_01_17/lib/dojo20170117.ex
+++ 2018_01_17/lib/dojo20170117.ex
@@ -1,5 +1,17 @@
 defmodule Dojo do
 
+  def full_solve(field) do
+    field
+    |> slots_map(fn
+      "*", _x, _y -> "*"
+      ".", x, y ->
+        field
+        |> slots_around(x, y)
+        |> Enum.count(fn slot -> slot == "*" end)
+        |> to_string
+    end)
+  end
+
   def solve(field) do
     field
     |> Enum.with_index
@@ -14,6 +26,26 @@ defmodule Dojo do
       else
         "*"
       end
+    end)
+  end
+
+  defp slots_map(field, function) do
+    field
+    |> Enum.with_index
+    |> Enum.map(fn {line, y} ->
+      line
+      |> Enum.with_index
+      |> Enum.map(fn {slot, x} ->
+        function.(slot, x, y)
+      end)
+    end)
+  end
+
+  defp slots_around(field, x, y) do
+    field
+    |> Enum.slice(Enum.max([0,y-1])..(y+1))
+    |> Enum.flat_map(fn row ->
+      Enum.slice(row, Enum.max([0,x-1])..(x+1))
     end)
   end
 end
--- 2018_01_17/test/dojo20170117_test.exs
+++ 2018_01_17/test/dojo20170117_test.exs
@@ -1,34 +1,73 @@
 defmodule Dojo20170117Test do
   use ExUnit.Case
 
-  test "matrix 2x1 without mines" do
-  	solution = Dojo.solve([".","."])
-    assert solution == [0, 0]
+  describe "simple solve" do
+    test "matrix 2x1 without mines" do
+      solution = Dojo.solve([".","."])
+      assert solution == [0, 0]
+    end
+
+    test "matrix 2x1 with one mine" do
+      solution = Dojo.solve([".","*"])
+      assert solution == [1, "*"]
+    end
+
+    test "matrix 2x1 with two mines" do
+      solution = Dojo.solve(["*","*"])
+      assert solution == ["*", "*"]
+    end
+
+    test "matrix 2x1 with one mine 2" do
+      solution = Dojo.solve(["*","."])
+      assert solution == ["*", 1]
+    end
   end
 
-  test "matrix 2x1 with one mine" do
-  	solution = Dojo.solve([".","*"])
-	assert solution == [1, "*"]
-  end
+  describe "full solve" do
+    test "matrix 2x2 full of mines" do
+      solution = Dojo.full_solve([
+        ~w[* *],
+        ~w[* *],
+      ])
+      assert solution == [
+        ~w[* *],
+        ~w[* *],
+      ]
+    end
 
-  test "matrix 2x1 with two mines" do
-  	solution = Dojo.solve(["*","*"])
-	assert solution == ["*", "*"]
-  end
+    test "matrix 2x2 with three mines" do
+      solution = Dojo.full_solve([
+        ~w[. *],
+        ~w[* *],
+      ])
+      assert solution == [
+        ~w[3 *],
+        ~w[* *],
+      ]
+    end
 
-  test "matrix 2x1 with one mine 2" do
-  	solution = Dojo.solve(["*","."])
-	assert solution == ["*", 1]
-  end
+    test "matrix 2x2 with no mines" do
+      solution = Dojo.full_solve([
+        ~w[. .],
+        ~w[. .],
+      ])
+      assert solution == [
+        ~w[0 0],
+        ~w[0 0],
+      ]
+    end
 
-  # test "matrix 2x2 with two mines" do
-  # 	solution = Dojo.solve([
-  # 		["*", "*"],
-  # 		[".", "."]
-  # 	])
-  # 	assert solution == [
-  # 		["*", "*"],
-  # 		[2  , 	2]
-  # 	]
-  # end
+    test "matrix 3x3 with 3 mines" do
+      solution = Dojo.full_solve([
+        ~w[* * .],
+        ~w[* . .],
+        ~w[. . .],
+      ])
+      assert solution == [
+        ~w[* * 1],
+        ~w[* 3 1],
+        ~w[1 1 0],
+      ]
+    end
+  end
 end
