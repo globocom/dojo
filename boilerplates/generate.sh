07415b591414697232307daa4b07ab25af9b867f
Cristian Dean <cristian.dean@corp.globo.com>
2019-10-01 09:58:05 -0300

    Improve boilerplate generator and change language to english

--- boilerplates/generate.sh
+++ boilerplates/generate.sh
@@ -7,19 +7,23 @@ JAVASCRIPT="javascript"
 
 ALLOWED_LANGUAGES=($GO_LANG $PYTHON $JAVA $JAVASCRIPT)
 
+GREEN='\033[0;32m' 
+RED='\033[0;31m' 
+NOCOLOR='\033[0m' 
+
 echo ""
 
 if [[ ! " ${ALLOWED_LANGUAGES[*]} " == *" $1 "* ]]; then
-    echo "Linguagem inválida!!"
-    echo "Apenas as linguagens a seguir são válidas!"
-    echo "LINGUAGENS: ${ALLOWED_LANGUAGES[*]}"
+    echo "Invalid language!!"
+    echo "Use one of the languages below!"
+    echo "${GREEN}${ALLOWED_LANGUAGES[*]}${NOCOLOR}"
     exit 0
 fi
 
 
 # Linguagem escolhida
 language=$1
-echo "Linguagem escolhida ${language}"
+echo "Language selected: ${language}"
 echo ""
 
 # Retorno: Uma data no formato yyyy-mm-dd
@@ -29,26 +33,28 @@ currentDate=`date +%F`
 dojoDir="${currentDate//-/_}"
 
 
-echo "Criando o diretório..."
+echo "Creating directory..."
 echo ""
 
 errorDir="$(mkdir $dojoDir 2>&1)"
 
 if [ "$errorDir" != "" ]; then
-    echo "Erro: ${errorDir}"
+    echo "${RED}Erro: ${errorDir}${NOCOLOR}"
     echo ""
     exit 0
 fi
 
-echo "Copiando arquivos para a pasta..."
+echo "Copying the files to the directory ./${dojoDir} ..."
 echo ""
 
 erroCp=$(cp -R boilerplates/$1/. $dojoDir 2>&1)
 
 if [ "$erroCp" != "" ]; then
-    echo "Erro: ${erroCp}"
+    echo "${RED}Erro: ${erroCp}${NOCOLOR}"
     echo ""
     exit 0
 fi
+
+echo "Boilerplate successfully created: $GREEN$dojoDir$NOCOLOR"
 
 cat TITLE
