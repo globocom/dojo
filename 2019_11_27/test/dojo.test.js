f909e7b92270d4d21dc5c0845d6250939de3117f
Cristian Dean <cristian.dean@corp.globo.com>
2019-12-04 18:02:58 -0300

    Dojo do dia 27/11/2019

---
+++ 2019_11_27/test/dojo.test.js
@@ -0,0 +1,8 @@
+var assert = require('assert');
+var dojo = require('../src/dojo');
+
+describe('Test True', function () {
+ it('should return true', function () {
+        assert.equal(dojo(), true);
+    });
+});
